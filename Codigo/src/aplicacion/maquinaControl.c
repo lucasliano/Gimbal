40accf10c248a3304f9caaed873be1db98e74f2a
Lucas Liaño <lliano@est.frba.utn.edu.ar>
2019-07-23 19:26:53 -0300

    Agregue mas recursos
    Meto las carpetas de documentación y de las piezas 3D.

---
+++ Codigo/src/aplicacion/maquinaControl.c
@@ -0,0 +1,27 @@
+/**
+  \file maquinaControl.c
+  \brief Se encuentran las rutinas relacionadas con el sistema de control.
+  \author Grupo 8 - R2003
+  \date 2019.07.23
+  \version 1.1
+*/
+
+#include "headers/aplicacion/maquinaControl.h"
+
+/**
+  \fn void Maquina_Medir();
+  \brief Se encarga de llamar por primera vez a los tres timers TIMER_DEC, TIMER_SEG, TIMER_MIN.
+  \author Grupo 8 - R2003
+  \date 2019.07.23
+*/
+bool Maquina_Medir()
+{}
+
+  /**
+    \fn void F_Calculando();
+    \brief Se encarga de llamar por primera vez a los tres timers TIMER_DEC, TIMER_SEG, TIMER_MIN.
+    \author Grupo 8 - R2003
+    \date 2019.07.23
+  */
+bool F_Calculando()
+{}
